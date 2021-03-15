2bdfcdda2f3a4241e2900f469145ab6de9109831
Elliot Thomas <elliot@eth0net.io>
2021-03-15 16:27:25 +0000

    Implemented testing for most ActTypes

--- systems/character/entity_test.go
+++ systems/character/entity_test.go
@@ -76,3 +76,155 @@ func TestSetAnimation(t *testing.T) {
 		})
 	}
 }
+
+func TestRunSchedule(t *testing.T) {
+	t.Parallel()
+
+	animations := map[string]*common.Animation{
+		AnimationMoveUp:    {Name: AnimationMoveUp, Loop: true},
+		AnimationMoveDown:  {Name: AnimationMoveDown, Loop: true},
+		AnimationMoveLeft:  {Name: AnimationMoveLeft, Loop: true},
+		AnimationMoveRight: {Name: AnimationMoveRight, Loop: true},
+		AnimationStopUp:    {Name: AnimationStopUp, Loop: true},
+		AnimationStopDown:  {Name: AnimationStopDown, Loop: true},
+		AnimationStopLeft:  {Name: AnimationStopLeft, Loop: true},
+		AnimationStopRight: {Name: AnimationStopRight, Loop: true},
+	}
+
+	char := characterEntity{
+		AnimationComponent: &common.AnimationComponent{
+			Animations: animations,
+		},
+		SpaceComponent:     &common.SpaceComponent{},
+		CharacterComponent: &CharacterComponent{},
+		SpeedComponent:     &speed.SpeedComponent{},
+	}
+
+	t.Run("ActStop", func(t *testing.T) {
+		char.SpeedComponent.Point = engo.Point{X: 1, Y: 1}
+		char.CharacterComponent.Schedule = Schedule{
+			Actions: []Action{
+				{Type: ActStop},
+			},
+		}
+		char.runSchedule(0)
+
+		want, got := engo.Point{}, char.SpeedComponent.Point
+		if !want.Equal(got) {
+			t.Errorf("wanted Point == %v, got %v", want, got)
+		}
+	})
+
+	t.Run("ActTurn", func(t *testing.T) {
+		char.SpeedComponent.Point = engo.Point{Y: 1}
+		char.CharacterComponent.Schedule = Schedule{
+			Actions: []Action{
+				{Type: ActTurn, Point: engo.Point{X: 1}},
+			},
+		}
+		char.AnimationComponent.SelectAnimationByName(AnimationMoveDown)
+		char.runSchedule(0)
+		char.setAnimation()
+
+		wantPoint, gotPoint := engo.Point{}, char.SpeedComponent.Point
+		if !wantPoint.Equal(gotPoint) {
+			t.Errorf("wanted Point == %v, got %v", wantPoint, gotPoint)
+		}
+
+		wantAnim := animations[AnimationStopRight]
+		gotAnim := char.AnimationComponent.CurrentAnimation
+		if wantAnim != gotAnim {
+			t.Errorf("wanted Animation == %v, got %v", wantAnim, gotAnim)
+		}
+	})
+
+	t.Run("ActWalk", func(t *testing.T) {
+		char.CharacterComponent.Schedule = Schedule{
+			Actions: []Action{
+				{Type: ActWalk, Point: engo.Point{X: 1}},
+			},
+		}
+		t.Skipf("not yet implemented")
+	})
+
+	t.Run("ActRun", func(t *testing.T) {
+		char.CharacterComponent.Schedule = Schedule{
+			Actions: []Action{
+				{Type: ActRun, Point: engo.Point{X: 1}},
+			},
+		}
+		t.Skipf("not yet implemented")
+	})
+
+	t.Run("ActTeleport", func(t *testing.T) {
+		char.SpaceComponent.Position = engo.Point{}
+		char.CharacterComponent.Schedule = Schedule{
+			Actions: []Action{
+				{Type: ActTeleport, Point: engo.Point{X: 1}},
+			},
+		}
+		char.runSchedule(1)
+
+		want, got := engo.Point{X: 1}, char.SpaceComponent.Position
+		if !want.Equal(got) {
+			t.Errorf("wanted Point == %v, got %v", want, got)
+		}
+	})
+
+	t.Run("ActTurnTo", func(t *testing.T) {
+		char.SpaceComponent.Position = engo.Point{}
+		char.SpeedComponent.Point = engo.Point{Y: 1}
+		char.CharacterComponent.Schedule = Schedule{
+			Actions: []Action{
+				{Type: ActTurnTo, Point: engo.Point{X: 1}},
+			},
+		}
+		char.AnimationComponent.SelectAnimationByName(AnimationMoveDown)
+		char.runSchedule(0)
+		char.setAnimation()
+
+		wantPoint, gotPoint := engo.Point{}, char.SpeedComponent.Point
+		if !wantPoint.Equal(gotPoint) {
+			t.Errorf("wanted Point == %v, got %v", wantPoint, gotPoint)
+		}
+
+		wantAnim := animations[AnimationStopRight]
+		gotAnim := char.AnimationComponent.CurrentAnimation
+		if wantAnim != gotAnim {
+			t.Errorf("wanted Animation == %v, got %v", wantAnim, gotAnim)
+		}
+	})
+
+	t.Run("ActWalkTo", func(t *testing.T) {
+		char.CharacterComponent.Schedule = Schedule{
+			Actions: []Action{
+				{Type: ActWalkTo, Point: engo.Point{X: 1}},
+			},
+		}
+		t.Skipf("not yet implemented")
+	})
+
+	t.Run("ActRunTo", func(t *testing.T) {
+		char.CharacterComponent.Schedule = Schedule{
+			Actions: []Action{
+				{Type: ActRunTo, Point: engo.Point{X: 1}},
+			},
+		}
+		t.Skipf("not yet implemented")
+	})
+
+	t.Run("ActTeleportTo", func(t *testing.T) {
+		char.SpaceComponent.Position = engo.Point{}
+		char.CharacterComponent.Schedule = Schedule{
+			Actions: []Action{
+				{Type: ActTeleportTo, Point: engo.Point{X: 1}},
+			},
+		}
+		char.runSchedule(1)
+
+		want, got := engo.Point{X: 1}, char.SpaceComponent.Position
+		if !want.Equal(got) {
+			t.Errorf("wanted Point == %v, got %v", want, got)
+		}
+	})
+}
